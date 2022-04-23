e47d6c64f6a048167ea53d0593fef4c2cd325a5d
Станислав Бескаев <sbeskaev@phoenixit.ru>
2022-04-23 21:54:26 +0300

    class_dependencies.py через модель

--- dependencies/class_dependencies.py
+++ dependencies/class_dependencies.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import Depends, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,17 +9,24 @@ app = FastAPI()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-class CommonQueryParams:
-    def __init__(self, q: Optional[str] = None, skip: int = 0, limit: int = 100):
-        self.q = q
-        self.skip = skip
-        self.limit = limit
+# Можно так указать, а можно через модель
+# class CommonQueryParams:
+#     def __init__(self, q: Optional[str] = None, skip: int = 0, limit: int = 100):
+#         self.q = q
+#         self.skip = skip
+#         self.limit = limit
+
+class CommonQueryParams(BaseModel):
+    q: Optional[str] = None
+    skip: int = 0
+    limit: int = 100
+    need: int
 
 
 @app.get("/items/")
 async def read_items(commons: CommonQueryParams = Depends(CommonQueryParams)):
     # можно указать commons: CommonQueryParams = Depends() и тоже будет работать
-    # благодаря Depends commons будет экземпляром класса Depends
+    # благодаря Depends commons будет экземпляром класса CommonQueryParams
     response = {}
     if commons.q:
         response.update({"q": commons.q})
