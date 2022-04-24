a8c03f3387c87dfaaa4f6d2b107e18379de291ed
Станислав Бескаев <sbeskaev@phoenixit.ru>
2022-04-24 21:48:19 +0300

    Описания в документации

--- bigger_applicatation_example/internal/admin.py
+++ bigger_applicatation_example/internal/admin.py
@@ -9,7 +9,8 @@ router = APIRouter()
 
 @router.post(
     "/",
-    dependencies=[Depends(check_auth)]
+    dependencies=[Depends(check_auth)],
+    description="Создание нового админа"
 )
 async def create_admin(new_admin: Admin, background_tasks: BackgroundTasks):
     AdminHandler.add_admin(new_admin)
@@ -17,20 +18,21 @@ async def create_admin(new_admin: Admin, background_tasks: BackgroundTasks):
     return {"message": f"Created new admin: {new_admin.username}"}
 
 
-@router.get("/username")
+@router.get("/username", description="Имя текущего админа")
 def read_admin_username(username: str = Depends(get_current_admin_username)):
     logger.debug(f"Пользователь {username} запросил имя пользователя")
     return {"admin": username}
 
 
-@router.get("/")
+@router.get("/", description="Список всех админов")
 async def admins_list():
     return [{"username": admin.username} for admin in AdminHandler.get_admins()]
 
 
 @router.delete(
     "/",
-    dependencies=[Depends(check_auth)]
+    dependencies=[Depends(check_auth)],
+    description="Удалить админа по имени"
 )
 async def delete_admin_by_name(username: str, background_tasks: BackgroundTasks):
     AdminHandler.delete_admin_by_username(username)
--- bigger_applicatation_example/main.py
+++ bigger_applicatation_example/main.py
@@ -4,7 +4,56 @@ from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import items, users
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+
+description = """
+Bigger application API example 🚀
+
+## Items
+Получение и обновление **items**
+
+## Users
+You will be able to:
+* **Get users**
+* **Read users** (_not implemented_).
+"""
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+    {
+        "name": "admin",
+        "description": "Admin CRUD implementation",
+    },
+    {
+        "name": "custom",
+        "description": "I've no idea what it is"
+    },
+    {
+        "name": "default",
+        "description": "Endpoints with no tags"
+    }
+]
+
+app = FastAPI(
+    title="Bigger application API",
+    dependencies=[Depends(get_query_token)],
+    description=description,
+    version="0.2.0",
+    openapi_tags=tags_metadata,
+    openapi_url="/api/v0.2.0/openapi.json",
+    docs_url="/swagger",
+    redoc_url="/redoc"
+)
 
 app.include_router(users.router)
 app.include_router(items.router)
