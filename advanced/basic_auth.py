3c20a02314734980581a159879768cc907305351
Станислав Бескаев <sbeskaev@phoenixit.ru>
2022-04-26 13:31:31 +0300

    Привет basic auth

---
+++ advanced/basic_auth.py
@@ -0,0 +1,31 @@
+import secrets
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+app = FastAPI()
+
+security = HTTPBasic()
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    # compare_digest, что бы защититься от timing attack
+    correct_username = secrets.compare_digest(credentials.username, "stanleyjobson")
+    correct_password = secrets.compare_digest(credentials.password, "swordfish")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.get("/users/me")
+def read_current_user(username: str = Depends(get_current_username)):
+    return {"username": username}
+
+
+@app.get("/info", dependencies=[Depends(security)])
+def get_info():
+    return {"message": "important"}
