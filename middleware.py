dad0f9cc9d0e3d1c7b0e5eabfa748b1181bd9b55
Станислав Бескаев <sbeskaev@phoenixit.ru>
2022-04-26 16:17:55 +0300

    небольшая обработка ошибок

--- middleware.py
+++ middleware.py
@@ -1,6 +1,7 @@
 import time
 
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from loguru import logger
 
 app = FastAPI()
@@ -14,7 +15,6 @@ async def add_process_time_header(request: Request, call_next):
     start_time = time.time()
 
     logger.debug("middleware before await")
-    logger.debug(f"{request.__dict__=}")
     response = await call_next(request)
     logger.debug("middleware after await")
 
@@ -25,7 +25,26 @@ async def add_process_time_header(request: Request, call_next):
     return response
 
 
-@app.get("/some")
-def some():
+@app.middleware("http")
+async def add_some_header(request: Request, call_next):
+    response = await call_next(request)
+    if response.status_code == 200:
+        logger.debug("add some header")
+        response.headers["X-some"] = "thing"
+
+    return response
+
+
+@app.exception_handler(ValueError)
+def handle_wrong_pk(request: Request, exc: ValueError):
+    logger.debug("handle_wrong_pk")
+    response = JSONResponse(content={"message": "pk must be one"}, status_code=400)
+    return response
+
+
+@app.get("/some/{pk}")
+def some(pk: int):
+    if pk != 1:
+        raise ValueError("pk is not one!")
     logger.debug(f"request to /some")
     return {"some": "thing"}
